a180cfed097ede7a99fa0a446bf821d6dddf5c73
“ShabinMuhammedKK” <shabinmuhammedkk@gmail.com>
2024-01-02 21:09:21 +0530

    task adding

--- src/component/TodoApp/TodoApp.js
+++ src/component/TodoApp/TodoApp.js
@@ -1,37 +1,54 @@
 import React, { Component } from "react";
 import "./TodoApp.css";
-import LibraryAddCheckRoundedIcon from '@mui/icons-material/LibraryAddCheckRounded';
-import QueueRoundedIcon from '@mui/icons-material/QueueRounded';
-import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 
 export class TodoApp extends Component {
+  state = {
+    input: "",
+    items :[],
+  };
+  handleChange = (event) => {
+    this.setState({
+      input : event.target.value
+    });
+  };
+  storeItems = (event)=>{
+    event.preventDefault();
+    const {input} = this.state;
+
+    this.setState({
+      items:[...this.state.items,input],
+      input:""
+    })
+  }
+  deleteItems = (index)=>{
+   this.state.items.splice(index,1);
+   this.setState({
+    items:[...this.state.items]
+   })
+  }
   render() {
+    const {input,items}= this.state;
     return (
       <div className="todo-container">
-        <h1>TODO APP <span className="menubar"><MenuRoundedIcon/></span></h1>
+        <h1>TODO APP</h1>
+        <form action="" onSubmit={this.storeItems}>
         <div className="input-section">
-          <input className="input-col" type="text" />
-          <button className="add-btn"><QueueRoundedIcon/></button>
+          <input className="input-col" type="text" value={input} onChange={this.handleChange} placeholder="add new tasks..." />
+          <button className="add-btn">
+            <i class="fa-solid fa-plus"></i>
+          </button>
         </div>
+        </form>
         <ul>
-          <li>
-            <span className="item-text">Item 1</span>
+          {items.map((data,index)=>(
+
+          <li key={index}>
+            <span className="item-text">{data}</span>
             <span className="delete-btn">
-              <LibraryAddCheckRoundedIcon />
+            <i class="fa-solid fa-trash" onClick={()=>this.deleteItems(index)}></i>
             </span>
           </li>
-          <li>
-            <span className="item-text">Item 2</span>
-            <span className="delete-btn">
-              <LibraryAddCheckRoundedIcon />
-            </span>
-          </li>
-          <li>
-            <span className="item-text">Item 3</span>
-            <span className="delete-btn">
-              <LibraryAddCheckRoundedIcon />
-            </span>
-          </li>
+          ))}
         </ul>
       </div>
     );
